2e373a04d66a5e20254b5008fe7becc2d19d6035
小白龙 <hyaxie@163.com>
2019-11-09 23:29:47 +0800

    feat: 增加webpack配置

---
+++ webpack-learn/learn1/app.js
@@ -0,0 +1,17 @@
+import './index.less'
+
+class MyApp {
+  // state = 1
+  add() {
+    return 1
+  }
+  plus() {
+    return 367
+  }
+}
+
+const ace = new MyApp()
+ace.add()
+ace.plus()
+
+export default MyApp
