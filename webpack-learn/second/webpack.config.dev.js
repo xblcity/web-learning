c7c8cf7b6d7a174d5d1cf79ce379ddc2e289c921
xblcity <hyaxie@163.com>
2020-04-13 23:09:49 +0800

    feat: 修改devServer配置

--- webpack-learn/second/webpack.config.dev.js
+++ webpack-learn/second/webpack.config.dev.js
@@ -8,11 +8,9 @@ module.exports = merge(baseWebpackConfig, {
     devtool: 'source-map',
     // 配置dev-server
     devServer: {
-      contentBase: './src', // 告诉devServer监听哪些文件的变化
+      contentBase: './dist', // 根目录
+      open: true,
       port: '3000', //默认是8080
-      quiet: false, //默认不启用
-      inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
       stats: "errors-only", //终端仅打印 error
-      compress: true //是否启用 gzip 压缩
   }
 });
